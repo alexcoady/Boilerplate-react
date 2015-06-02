ef35b164c97a7a975a72fb3707816a01af899546
Alex Coady <alexcoady@stinkdigital.com>
2015-06-02 15:02:39 +0100

    jquery dep

--- source/scripts/commentBox.jsx
+++ source/scripts/commentBox.jsx
@@ -1,4 +1,5 @@
 var React = require("react/addons");
+var $ = require("jquery");
 
 var CommentList = require("./commentList.jsx");
 var CommentForm = require("./commentForm.jsx");
@@ -7,12 +8,24 @@ var CommentForm = require("./commentForm.jsx");
 var CommentBox = React.createClass({
 
 
+  getInitialState: function () {
+
+    return { data: [] };
+  },
+
+
+  componentDidMount: function () {
+
+
+  },
+
+
   render: function () {
 
     return (
       <div className="CommentBox">
         <h1>Comments</h1>
-          <div className="CommentBox-commentList"><CommentList data={this.props.data} /></div>
+          <div className="CommentBox-commentList"><CommentList data={this.state.data} /></div>
           <div className="CommentBox-commentForm"><CommentForm /></div>
       </div>
     );
